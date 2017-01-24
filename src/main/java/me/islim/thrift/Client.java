ed3ca54325d00905c89b8e5716861d2f05759b11
liuxiaozhu <963214>
2017-01-24 16:39:53 +0800

    各服务模型

--- src/main/java/me/islim/thrift/Client.java
+++ src/main/java/me/islim/thrift/Client.java
@@ -1,11 +1,17 @@
 package me.islim.thrift;
 
 import org.apache.thrift.TException;
+import org.apache.thrift.async.AsyncMethodCallback;
+import org.apache.thrift.async.TAsyncClientManager;
 import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TCompactProtocol;
 import org.apache.thrift.protocol.TProtocol;
-import org.apache.thrift.transport.TSocket;
-import org.apache.thrift.transport.TTransport;
-import org.apache.thrift.transport.TTransportException;
+import org.apache.thrift.protocol.TProtocolFactory;
+import org.apache.thrift.transport.*;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class Client {
 
@@ -13,8 +19,15 @@ public class Client {
 
     private static final int PORT = 1234;
 
-    public static void main(String[] args) throws TException {
+    private static final int TIMEOUT = 30000;
 
+    public static void main(String[] args) throws TException {
+//        startThreadPoolClient();
+//        startNonblockingClient();
+        startAsyncClient();
+    }
+
+    public static void startThreadPoolClient(){
         TTransport transport = new TSocket(HOST, PORT);
 
         TProtocol protocol = new TBinaryProtocol(transport);
@@ -33,5 +46,90 @@ public class Client {
             e.printStackTrace();
         }
         transport.close();
+    }
+
+    public static void startNonblockingClient(){
+        TTransport transport = new TFramedTransport(new TSocket(HOST, PORT, TIMEOUT));
+
+        TProtocol protocol = new TCompactProtocol(transport);
+
+        Test.Client client = new Test.Client(protocol);
+        try {
+            transport.open();
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        }
+        try {
+            client.ping(2017);
+        } catch (TException e) {
+            e.printStackTrace();
+        }
+        transport.close();
+    }
+
+    public static void startAsyncClient(){
+        //异步调用管理器
+        TAsyncClientManager clientManager = null;
+        try {
+            clientManager = new TAsyncClientManager();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //设置传输通道，调用非阻塞IO
+        TNonblockingTransport transport = null;
+        try {
+            transport = new TNonblockingSocket(HOST, PORT, TIMEOUT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 协议要和服务端一致
+        //TProtocolFactory tprotocol = new TCompactProtocol.Factory();
+        TProtocolFactory tprotocol = new TBinaryProtocol.Factory();
+
+        Test.AsyncClient asyncClient = new Test.AsyncClient(tprotocol, clientManager, transport);
+        CountDownLatch latch = new CountDownLatch(1);
+        AsyncMethodCallback callBack = new AsynCallback(latch);
+        // 调用服务
+        try {
+            asyncClient.ping(2017, callBack);
+        } catch (TException e) {
+            e.printStackTrace();
+        }
+        //等待完成异步调用
+        boolean wait = false;
+        try {
+            wait = latch.await(30, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class AsynCallback implements AsyncMethodCallback<Test.AsyncClient.ping_call> {
+    private CountDownLatch latch;
+
+    public AsynCallback(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @Override
+    public void onComplete(Test.AsyncClient.ping_call response) {
+        try {
+            // response is null
+            if(response != null)
+                System.out.println("AsynCall result :" + response.getResult().toString());
+        } catch (TException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            latch.countDown();
+        }
+    }
+
+    @Override
+    public void onError(Exception exception) {
+        System.out.println("onError :" + exception.getMessage());
+        latch.countDown();
     }
 }
--- src/main/java/me/islim/thrift/Server.java
+++ src/main/java/me/islim/thrift/Server.java
@@ -2,37 +2,121 @@ package me.islim.thrift;
 
 
 import me.islim.thrift.impl.TestImpl;
+import org.apache.thrift.TProcessor;
 import org.apache.thrift.protocol.TBinaryProtocol;
-import org.apache.thrift.server.TServer;
-import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.server.*;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingServerSocket;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TTransportException;
 
+/**
+ * Underlying I/O: 底层传输
+ * Transport: 传输层
+ * protocol: 协议层
+ * Processor: 处理层
+ * Server: 服务层
+ * Your Code: 业务逻辑层
+ */
 public class Server {
 
     private static final int PORT = 1234;
 
-    public void startServer(){
-        try{
-            TServerSocket serverTransport = new TServerSocket(PORT);
+    /**
+     * 多线程阻塞式IO服务模型-TThreadPoolServer
+     */
+    public static void startThreadPoolServer() {
 
-            Test.Processor process = new Test.Processor(new TestImpl());
+        Test.Processor process = new Test.Processor<Test.Iface>(new TestImpl());
 
-            TBinaryProtocol.Factory portFactory = new TBinaryProtocol.Factory(true, true);
-
-            TThreadPoolServer.Args args = new TThreadPoolServer.Args(serverTransport);
-            args.processor(process);
-            args.protocolFactory(portFactory);
-
-            TServer server = new TThreadPoolServer(args);
-            server.serve();
-        }catch (TTransportException e){
+        TServerSocket serverTransport = null;
+        try {
+            serverTransport = new TServerSocket(PORT);
+        } catch (TTransportException e) {
             e.printStackTrace();
         }
+        TBinaryProtocol.Factory portFactory = new TBinaryProtocol.Factory(true, true);
+
+        TThreadPoolServer.Args args = new TThreadPoolServer.Args(serverTransport);
+        args.processor(process);
+        args.protocolFactory(portFactory);
+
+        TServer server = new TThreadPoolServer(args);
+        server.serve();
     }
 
-    public static void main(String[] args){
-        Server server = new Server();
-        server.startServer();
+    /**
+     * 多线程半同步半异步的服务模型-TThreadedSelectorServer
+     */
+    public static void startThreadedSelectorServer(){
+        TProcessor tprocessor = new Test.Processor<Test.Iface>(new TestImpl());
+
+        TNonblockingServerSocket serverTransport = null;
+        try {
+            serverTransport = new TNonblockingServerSocket(PORT);
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        }
+
+        TThreadedSelectorServer.Args tArgs = new TThreadedSelectorServer.Args(serverTransport);
+        tArgs.processor(tprocessor);
+        tArgs.transportFactory(new TFramedTransport.Factory());
+
+        tArgs.protocolFactory(new TBinaryProtocol.Factory());
+
+        TServer server = new TThreadedSelectorServer(tArgs);
+        server.serve();
+    }
+
+    /**
+     * 半同步半异步的服务模型-THsHaServer（异步调用客户端）
+     */
+    public static void startHsHaServer(){
+        TProcessor tprocessor = new Test.Processor<Test.Iface>(new TestImpl());
+        TNonblockingServerSocket serverTransport = null;
+        try {
+            serverTransport = new TNonblockingServerSocket(PORT);
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        }
+
+        THsHaServer.Args tArgs = new THsHaServer.Args(serverTransport);
+        tArgs.processor(tprocessor);
+        tArgs.transportFactory(new TFramedTransport.Factory());
+        tArgs.protocolFactory(new TBinaryProtocol.Factory());
+
+        TServer server = new THsHaServer(tArgs);
+        server.serve();
+    }
+
+    /**
+     * 非阻塞式IO服务模型-TNonblockingServer
+     */
+    public static void startNonblockingServer() {
+
+        Test.Processor process = new Test.Processor<Test.Iface>(new TestImpl());
+
+        TNonblockingServerSocket serverTransport = null;
+        try {
+            serverTransport = new TNonblockingServerSocket(PORT);
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        }
+
+        TNonblockingServer.Args tArgs = new TNonblockingServer.Args(serverTransport);
+        tArgs.processor(process);
+        tArgs.transportFactory(new TFramedTransport.Factory());
+        tArgs.protocolFactory(new TCompactProtocol.Factory());
+
+        TServer server = new TNonblockingServer(tArgs);
+        server.serve();
+    }
+
+    public static void main(String[] args) {
+//        startThreadPoolServer();
+//        startNonblockingServer();
+//        startHsHaServer();
+        startThreadedSelectorServer();
     }
 }
